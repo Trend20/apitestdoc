5a8ca4fabbea459ebfdac5cb201612198a7fb275
Trend20 <enockomondi305@gmail.com>
2024-09-09 11:25:58 +0300

    adding the root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,58 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command
+var rootCmd = &cobra.Command{
+	Use:   "apitester",
+	Short: "A CLI tool that tests APIs and generates API documentation.",
+	Long: `A CLI tool that tests APIs and generates API documentation based on the test results.
+Support's' different formats like OpenAPI/Swagger and includes automated testing features.`,
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	// Global flags
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ./configs/config.yaml)")
+
+	// Add additional commands here
+	rootCmd.AddCommand(testCmd)
+	rootCmd.AddCommand(generateDocCmd)
+
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search config in default locations
+		viper.AddConfigPath("./configs")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("No config file found, using defaults")
+	} else {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
